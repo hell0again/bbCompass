fc3de41cf4fa5c17f3977738004a902ebe0b98c5
hell0again <hell00again@gmail.com>
2016-01-23 23:25:04 +0900

    [master] ./run_and_push.sh "git checkout origin/feature/current_nodata public/feature/current_nodata && git add public/feature/current_nodata"

--- feature/current_nodata/bundle.js
+++ feature/current_nodata/bundle.js
@@ -47,7 +47,7 @@
 	__webpack_require__(1);
 	__webpack_require__(5);
 	__webpack_require__(12);
-	module.exports = __webpack_require__(18);
+	module.exports = __webpack_require__(19);
 
 
 /***/ },
@@ -61,7 +61,7 @@
 	    value: true
 	});
 
-	var _get = function get(_x2, _x3, _x4) { var _again = true; _function: while (_again) { var object = _x2, property = _x3, receiver = _x4; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x2 = parent; _x3 = property; _x4 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+	var _get = function get(_x2, _x3, _x4) { var _again = true; _function: while (_again) { var object = _x2, property = _x3, receiver = _x4; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x2 = parent; _x3 = property; _x4 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
 	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
@@ -14301,10 +14301,11 @@
 	var _lodash2 = _interopRequireDefault(_lodash);
 
 	/*global require*/ // eslint
-	// var Map = require('es6-map');
-	var currentJson = __webpack_require__(15);
-	var stageJson = __webpack_require__(16);
-	var mapJson = __webpack_require__(17);
+	//var Map = require('es6-map');
+	__webpack_require__(15);
+	var currentJson = __webpack_require__(16);
+	var stageJson = __webpack_require__(17);
+	var mapJson = __webpack_require__(18);
 
 	// NOTE: currentMapはこの順序で並べる
 	var currentMapEx = [{ "type": "national_battle_high", "label": "▼ 全国対戦(上位) ▼" }, { "type": "event", "label": "▼ イベントバトル ▼" }, { "type": "union_battle", "label": "▼ ユニオンバトル ▼" }, { "type": "scramble_battle", "label": "▼ スクランブルバトル ▼" }, { "type": "squad_battle", "label": "▼ スカッドバトル ▼" }, { "type": "national_battle_low", "label": "▼ 全国対戦(下位) ▼" }, { "type": "undefined", "label": "▼ その他 ▼" }];
@@ -26855,6 +26856,241 @@
 /* 15 */
 /***/ function(module, exports) {
 
+	/* WEBPACK VAR INJECTION */(function(global) {(function (exports) {'use strict';
+	  //shared pointer
+	  var i;
+	  //shortcuts
+	  var defineProperty = Object.defineProperty, is = function(a,b) { return isNaN(a)? isNaN(b): a === b; };
+
+
+	  //Polyfill global objects
+	  if (typeof WeakMap == 'undefined') {
+	    exports.WeakMap = createCollection({
+	      // WeakMap#delete(key:void*):boolean
+	      'delete': sharedDelete,
+	      // WeakMap#clear():
+	      clear: sharedClear,
+	      // WeakMap#get(key:void*):void*
+	      get: sharedGet,
+	      // WeakMap#has(key:void*):boolean
+	      has: mapHas,
+	      // WeakMap#set(key:void*, value:void*):void
+	      set: sharedSet
+	    }, true);
+	  }
+
+	  if (typeof Map == 'undefined' || typeof ((new Map).values) !== 'function' || !(new Map).values().next) {
+	    exports.Map = createCollection({
+	      // WeakMap#delete(key:void*):boolean
+	      'delete': sharedDelete,
+	      //:was Map#get(key:void*[, d3fault:void*]):void*
+	      // Map#has(key:void*):boolean
+	      has: mapHas,
+	      // Map#get(key:void*):boolean
+	      get: sharedGet,
+	      // Map#set(key:void*, value:void*):void
+	      set: sharedSet,
+	      // Map#keys(void):Iterator
+	      keys: sharedKeys,
+	      // Map#values(void):Iterator
+	      values: sharedValues,
+	      // Map#entries(void):Iterator
+	      entries: mapEntries,
+	      // Map#forEach(callback:Function, context:void*):void ==> callback.call(context, key, value, mapObject) === not in specs`
+	      forEach: sharedForEach,
+	      // Map#clear():
+	      clear: sharedClear
+	    });
+	  }
+
+	  if (typeof Set == 'undefined' || typeof ((new Set).values) !== 'function' || !(new Set).values().next) {
+	    exports.Set = createCollection({
+	      // Set#has(value:void*):boolean
+	      has: setHas,
+	      // Set#add(value:void*):boolean
+	      add: sharedAdd,
+	      // Set#delete(key:void*):boolean
+	      'delete': sharedDelete,
+	      // Set#clear():
+	      clear: sharedClear,
+	      // Set#keys(void):Iterator
+	      keys: sharedValues, // specs actually say "the same function object as the initial value of the values property"
+	      // Set#values(void):Iterator
+	      values: sharedValues,
+	      // Set#entries(void):Iterator
+	      entries: setEntries,
+	      // Set#forEach(callback:Function, context:void*):void ==> callback.call(context, value, index) === not in specs
+	      forEach: sharedForEach
+	    });
+	  }
+
+	  if (typeof WeakSet == 'undefined') {
+	    exports.WeakSet = createCollection({
+	      // WeakSet#delete(key:void*):boolean
+	      'delete': sharedDelete,
+	      // WeakSet#add(value:void*):boolean
+	      add: sharedAdd,
+	      // WeakSet#clear():
+	      clear: sharedClear,
+	      // WeakSet#has(value:void*):boolean
+	      has: setHas
+	    }, true);
+	  }
+
+
+	  /**
+	   * ES6 collection constructor
+	   * @return {Function} a collection class
+	   */
+	  function createCollection(proto, objectOnly){
+	    function Collection(a){
+	      if (!this || this.constructor !== Collection) return new Collection(a);
+	      this._keys = [];
+	      this._values = [];
+	      this._itp = []; // iteration pointers
+	      this.objectOnly = objectOnly;
+
+	      //parse initial iterable argument passed
+	      if (a) init.call(this, a);
+	    }
+
+	    //define size for non object-only collections
+	    if (!objectOnly) {
+	      defineProperty(proto, 'size', {
+	        get: sharedSize
+	      });
+	    }
+
+	    //set prototype
+	    proto.constructor = Collection;
+	    Collection.prototype = proto;
+
+	    return Collection;
+	  }
+
+
+	  /** parse initial iterable argument passed */
+	  function init(a){
+	    var i;
+	    //init Set argument, like `[1,2,3,{}]`
+	    if (this.add)
+	      a.forEach(this.add, this);
+	    //init Map argument like `[[1,2], [{}, 4]]`
+	    else
+	      a.forEach(function(a){this.set(a[0],a[1])}, this);
+	  }
+
+
+	  /** delete */
+	  function sharedDelete(key) {
+	    if (this.has(key)) {
+	      this._keys.splice(i, 1);
+	      this._values.splice(i, 1);
+	      // update iteration pointers
+	      this._itp.forEach(function(p) { if (i < p[0]) p[0]--; });
+	    }
+	    // Aurora here does it while Canary doesn't
+	    return -1 < i;
+	  };
+
+	  function sharedGet(key) {
+	    return this.has(key) ? this._values[i] : undefined;
+	  }
+
+	  function has(list, key) {
+	    if (this.objectOnly && key !== Object(key))
+	      throw new TypeError("Invalid value used as weak collection key");
+	    //NaN or 0 passed
+	    if (key != key || key === 0) for (i = list.length; i-- && !is(list[i], key);){}
+	    else i = list.indexOf(key);
+	    return -1 < i;
+	  }
+
+	  function setHas(value) {
+	    return has.call(this, this._values, value);
+	  }
+
+	  function mapHas(value) {
+	    return has.call(this, this._keys, value);
+	  }
+
+	  /** @chainable */
+	  function sharedSet(key, value) {
+	    this.has(key) ?
+	      this._values[i] = value
+	      :
+	      this._values[this._keys.push(key) - 1] = value
+	    ;
+	    return this;
+	  }
+
+	  /** @chainable */
+	  function sharedAdd(value) {
+	    if (!this.has(value)) this._values.push(value);
+	    return this;
+	  }
+
+	  function sharedClear() {
+	    (this._keys || 0).length =
+	    this._values.length = 0;
+	  }
+
+	  /** keys, values, and iterate related methods */
+	  function sharedKeys() {
+	    return sharedIterator(this._itp, this._keys);
+	  }
+
+	  function sharedValues() {
+	    return sharedIterator(this._itp, this._values);
+	  }
+
+	  function mapEntries() {
+	    return sharedIterator(this._itp, this._keys, this._values);
+	  }
+
+	  function setEntries() {
+	    return sharedIterator(this._itp, this._values, this._values);
+	  }
+
+	  function sharedIterator(itp, array, array2) {
+	    var p = [0], done = false;
+	    itp.push(p);
+	    return {
+	      next: function() {
+	        var v, k = p[0];
+	        if (!done && k < array.length) {
+	          v = array2 ? [array[k], array2[k]]: array[k];
+	          p[0]++;
+	        } else {
+	          done = true;
+	          itp.splice(itp.indexOf(p), 1);
+	        }
+	        return { done: done, value: v };
+	      }
+	    };
+	  }
+
+	  function sharedSize() {
+	    return this._values.length;
+	  }
+
+	  function sharedForEach(callback, context) {
+	    var it = this.entries();
+	    for (;;) {
+	      var r = it.next();
+	      if (r.done) break;
+	      callback.call(context, r.value[1], r.value[0], this);
+	    }
+	  }
+
+	})(typeof exports != 'undefined' && typeof global != 'undefined' ? global : window );
+
+	/* WEBPACK VAR INJECTION */}.call(exports, (function() { return this; }())))
+
+/***/ },
+/* 16 */
+/***/ function(module, exports) {
+
 	module.exports = [
 		{
 			"start_time": "2016-01-18",
@@ -26906,7 +27142,7 @@
 	];
 
 /***/ },
-/* 16 */
+/* 17 */
 /***/ function(module, exports) {
 
 	module.exports = [
@@ -27081,7 +27317,7 @@
 	];
 
 /***/ },
-/* 17 */
+/* 18 */
 /***/ function(module, exports) {
 
 	module.exports = [
@@ -28373,14 +28609,14 @@
 	];
 
 /***/ },
-/* 18 */
+/* 19 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
-	var _jquery = __webpack_require__(19);
+	var _jquery = __webpack_require__(20);
 
 	var _jquery2 = _interopRequireDefault(_jquery);
 
@@ -28403,9 +28639,9 @@
 	// require('jquery-ui/themes/base/theme.css');
 	// require('jquery-ui/themes/smoothness/jquery-ui.min.css');
 
-	__webpack_require__(20);
-
 	__webpack_require__(21);
+
+	__webpack_require__(22); /*global require*/ // eslint
 
 	//初期化
 	var CanvasName = "BBCompass";
@@ -29897,7 +30133,7 @@
 	// scrollLimitX, scrollLimitY,
 
 /***/ },
-/* 19 */
+/* 20 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*!
@@ -39113,7 +39349,7 @@
 
 
 /***/ },
-/* 20 */
+/* 21 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*
@@ -39352,19 +39588,19 @@
 
 	})(jQuery);
 
-	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(19)))
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(20)))
 
 /***/ },
-/* 21 */
+/* 22 */
 /***/ function(module, exports, __webpack_require__) {
 
 	// style-loader: Adds some css to the DOM by adding a <style> tag
 
 	// load the styles
-	var content = __webpack_require__(22);
+	var content = __webpack_require__(23);
 	if(typeof content === 'string') content = [[module.id, content, '']];
 	// add the styles to the DOM
-	var update = __webpack_require__(24)(content, {});
+	var update = __webpack_require__(25)(content, {});
 	if(content.locals) module.exports = content.locals;
 	// Hot Module Replacement
 	if(false) {
@@ -39381,10 +39617,10 @@
 	}
 
 /***/ },
-/* 22 */
+/* 23 */
 /***/ function(module, exports, __webpack_require__) {
 
-	exports = module.exports = __webpack_require__(23)();
+	exports = module.exports = __webpack_require__(24)();
 	// imports
 
 
@@ -39395,7 +39631,7 @@
 
 
 /***/ },
-/* 23 */
+/* 24 */
 /***/ function(module, exports) {
 
 	/*
@@ -39451,7 +39687,7 @@
 
 
 /***/ },
-/* 24 */
+/* 25 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/*
