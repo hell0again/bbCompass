784da52ffd3ff01c6d5f9936272f628b7eb69265
hell0again <hell00again@gmail.com>
2016-01-23 23:53:05 +0900

    [master] build

--- public/master/bundle.js
+++ public/master/bundle.js
@@ -47,7 +47,7 @@
 	__webpack_require__(1);
 	__webpack_require__(5);
 	__webpack_require__(12);
-	module.exports = __webpack_require__(18);
+	module.exports = __webpack_require__(19);
 
 
 /***/ },
@@ -14301,12 +14301,14 @@
 	var _lodash2 = _interopRequireDefault(_lodash);
 
 	/*global require*/ // eslint
-	var currentJson = __webpack_require__(15);
-	var stageJson = __webpack_require__(16);
-	var mapJson = __webpack_require__(17);
+	//var Map = require('es6-map');
+	__webpack_require__(15);
+	var currentJson = __webpack_require__(16);
+	var stageJson = __webpack_require__(17);
+	var mapJson = __webpack_require__(18);
 
 	// NOTE: currentMapはこの順序で並べる
-	var currentMapEx = [{ "type": "national_battle_high", "label": "▼ 全国対戦(上位) ▼" }, { "type": "event", "label": "▼ イベントバトル ▼" }, { "type": "union_battle", "label": "▼ ユニオンバトル ▼" }, { "type": "scramble_battle", "label": "▼ スクランブルバトル ▼" }, { "type": "squad_battle", "label": "▼ スカッドバトル ▼" }, { "type": "national_battle_low", "label": "▼ 全国対戦(下位) ▼" }];
+	var currentMapEx = [{ "type": "national_battle_high", "label": "▼ 全国対戦(上位) ▼" }, { "type": "event", "label": "▼ イベントバトル ▼" }, { "type": "union_battle", "label": "▼ ユニオンバトル ▼" }, { "type": "scramble_battle", "label": "▼ スクランブルバトル ▼" }, { "type": "squad_battle", "label": "▼ スカッドバトル ▼" }, { "type": "national_battle_low", "label": "▼ 全国対戦(下位) ▼" }, { "type": "undefined", "label": "▼ その他 ▼" }];
 	var BBDB = {
 	    // 現在の戦場
 	    "current_map": [
@@ -14380,72 +14382,56 @@
 	    });
 	})();
 	(function completeCurrentMap() {
-	    var i, il, key, type;
-	    var temps = {}; // {"belsk_f": {"mapName": "汀の", "stageId": "kinich", "stageName": "キニシュ"}, ...}
+	    var tempMap = new Map(); // 終わったらclearしないとリーク
 
-	    // // 例外対応分をマージ
-	    // _.each(currentMapEx, function(el, it) {
-	    //     var type = el["type"];
-	    //     if (false == el.hasOwnProperty("default")) {return;}
-	    //     var r = _.find(currentJson, function(el2) {
-	    //         return type == el2["type"];
-	    //     });
-	    //     if (r == undefined) {
-	    //         currentJson.push({
-	    //             "type": type,
-	    //             "maps": el["default"]
-	    //         });
-	    //     } else {
-	    //         r["maps"] = _.union( r["maps"], el["default"]);
-	    //     }
-	    // });
-
+	    // tempMap準備、日付関連
 	    _lodash2['default'].each(currentJson, function (el, it) {
 	        var start_date = new Date(el["start_time"]); // "2016-01-01" なら 2016/01/01 07:30:00 から。start_dateは補正後も日付が変わらないと仮定
 	        start_date.setMinutes(start_date.getMinutes() + 7 * 60 + 30);
 	        var end_date = new Date(el["end_time"]); // "2016-01-01" なら 2016/01/02 07:29:59 まで
 	        end_date.setMinutes(end_date.getMinutes() + 24 * 60 * 60 + 7 * 60 + 30);
-	        var prefix = "";
+
 	        if (end_date < Date.now()) {
 	            return;
-	        } else if (Date.now() < start_date) {
-	            prefix = "(" + start_date.getMonth() + 1 + "/" + start_date.getDate() + "〜) ";
-	        } else {
-	            // :
-	        }
-	        temps[el["map"]] = {
-	            "prefix": prefix
-	        };
-	    });
+	        } // 終了したマップは除外
 
-	    // mapとの紐付け
+	        var mv = {};
+	        tempMap.set(el, mv);
+
+	        if (Date.now() < start_date) {
+	            mv["datePrefix"] = start_date.getMonth() + 1 + "/" + start_date.getDate() + "〜";
+	        }
+	    });
+	    // mapとの紐付け。残念ながら O(map数 x tempMap数)
 	    _lodash2['default'].each(BBDB["map"], function (el, it) {
 	        var map = el;
-	        _lodash2['default'].each(temps, function (el2, it2) {
-	            var temp = el2;
-	            if (map["value"] == it2) {
-	                temp["mapName"] = map["text"];
-	                temp["stageId"] = map["dataset"]["stage"];
+	        tempMap.forEach(function (mv, mk) {
+	            if (mk["map"] != undefined && mk["map"] == map["value"]) {
+	                // mv["mapId"] = map["value"];
+	                mv["mapName"] = map["text"];
+	                mv["stageId"] = map["dataset"]["stage"];
 	            }
 	        });
 	    });
-	    // stageとの紐付け
+	    // stageとの紐付け。残念ながら O(stage数 x tempMap数)
 	    _lodash2['default'].each(BBDB["stage"], function (el, it) {
 	        var stage = el;
-	        _lodash2['default'].each(temps, function (el2, it2) {
-	            var temp = el2;
-	            if (stage["value"] === temp["stageId"]) {
-	                temp["stageName"] = stage["text"];
+	        tempMap.forEach(function (mv, mk) {
+	            if (mv["stageId"] != undefined && mv["stageId"] == stage["value"]) {
+	                mv["stageName"] = stage["text"];
 	            }
 	        });
 	    });
 	    // currentMapEx順で BBDB にpush
 	    _lodash2['default'].each(currentMapEx, function (el, it) {
-	        var type = el["type"];
-	        var maps = _lodash2['default'].filter(currentJson, function (el2) {
-	            return type == el2["type"];
+	        var mks = [];
+	        tempMap.forEach(function (mv, mk) {
+	            var t = mk.type || "undefined";
+	            if (t == el["type"]) {
+	                mks.push(mk);
+	            }
 	        });
-	        if (maps.length == 0) {
+	        if (mks.length == 0) {
 	            return;
 	        }
 
@@ -14456,18 +14442,36 @@
 	        });
 
 	        // mapsをpush
-	        _lodash2['default'].each(maps, function (el2, it2) {
-	            var mapId = el2["map"];
-	            var mapName = temps[mapId]["mapName"];
-	            // var stageId = temps[mapId]["stageId"];
-	            var stageName = temps[mapId]["stageName"];
-	            var labelPrefix = temps[mapId]["prefix"];
+	        _lodash2['default'].each(mks, function (mk) {
+	            var mv = tempMap.get(mk);
+	            var value = mk["map"];
+	            var label = "";
+	            var disabled = false;
+
+	            if (mv["datePrefix"] != undefined) {
+	                label = "[" + mv["datePrefix"] + "]";
+	            }
+	            if (mk["map"] == undefined) {
+	                value = "";
+	                label += "[NO DATA]";
+	                disabled = true;
+	            }
+	            if (mv["mapName"] == undefined || mv["stageName"] == undefined) {
+	                // 存在しないマップなら カレンダーのtitleを拾う
+	                label += mk["title"];
+	                disabled = true;
+	            } else {
+	                label += mv["mapName"] + " | " + mv["stageName"];
+	            }
+
 	            BBDB["current_map"].push({
-	                "value": mapId,
-	                "label": labelPrefix + mapName + " | " + stageName
+	                "value": value,
+	                "label": label,
+	                "disabled": disabled
 	            });
 	        });
 	    });
+	    tempMap.clear();
 	})();
 
 	exports['default'] = BBDB;
@@ -26852,6 +26856,241 @@
 /* 15 */
 /***/ function(module, exports) {
 
+	/* WEBPACK VAR INJECTION */(function(global) {(function (exports) {'use strict';
+	  //shared pointer
+	  var i;
+	  //shortcuts
+	  var defineProperty = Object.defineProperty, is = function(a,b) { return isNaN(a)? isNaN(b): a === b; };
+
+
+	  //Polyfill global objects
+	  if (typeof WeakMap == 'undefined') {
+	    exports.WeakMap = createCollection({
+	      // WeakMap#delete(key:void*):boolean
+	      'delete': sharedDelete,
+	      // WeakMap#clear():
+	      clear: sharedClear,
+	      // WeakMap#get(key:void*):void*
+	      get: sharedGet,
+	      // WeakMap#has(key:void*):boolean
+	      has: mapHas,
+	      // WeakMap#set(key:void*, value:void*):void
+	      set: sharedSet
+	    }, true);
+	  }
+
+	  if (typeof Map == 'undefined' || typeof ((new Map).values) !== 'function' || !(new Map).values().next) {
+	    exports.Map = createCollection({
+	      // WeakMap#delete(key:void*):boolean
+	      'delete': sharedDelete,
+	      //:was Map#get(key:void*[, d3fault:void*]):void*
+	      // Map#has(key:void*):boolean
+	      has: mapHas,
+	      // Map#get(key:void*):boolean
+	      get: sharedGet,
+	      // Map#set(key:void*, value:void*):void
+	      set: sharedSet,
+	      // Map#keys(void):Iterator
+	      keys: sharedKeys,
+	      // Map#values(void):Iterator
+	      values: sharedValues,
+	      // Map#entries(void):Iterator
+	      entries: mapEntries,
+	      // Map#forEach(callback:Function, context:void*):void ==> callback.call(context, key, value, mapObject) === not in specs`
+	      forEach: sharedForEach,
+	      // Map#clear():
+	      clear: sharedClear
+	    });
+	  }
+
+	  if (typeof Set == 'undefined' || typeof ((new Set).values) !== 'function' || !(new Set).values().next) {
+	    exports.Set = createCollection({
+	      // Set#has(value:void*):boolean
+	      has: setHas,
+	      // Set#add(value:void*):boolean
+	      add: sharedAdd,
+	      // Set#delete(key:void*):boolean
+	      'delete': sharedDelete,
+	      // Set#clear():
+	      clear: sharedClear,
+	      // Set#keys(void):Iterator
+	      keys: sharedValues, // specs actually say "the same function object as the initial value of the values property"
+	      // Set#values(void):Iterator
+	      values: sharedValues,
+	      // Set#entries(void):Iterator
+	      entries: setEntries,
+	      // Set#forEach(callback:Function, context:void*):void ==> callback.call(context, value, index) === not in specs
+	      forEach: sharedForEach
+	    });
+	  }
+
+	  if (typeof WeakSet == 'undefined') {
+	    exports.WeakSet = createCollection({
+	      // WeakSet#delete(key:void*):boolean
+	      'delete': sharedDelete,
+	      // WeakSet#add(value:void*):boolean
+	      add: sharedAdd,
+	      // WeakSet#clear():
+	      clear: sharedClear,
+	      // WeakSet#has(value:void*):boolean
+	      has: setHas
+	    }, true);
+	  }
+
+
+	  /**
+	   * ES6 collection constructor
+	   * @return {Function} a collection class
+	   */
+	  function createCollection(proto, objectOnly){
+	    function Collection(a){
+	      if (!this || this.constructor !== Collection) return new Collection(a);
+	      this._keys = [];
+	      this._values = [];
+	      this._itp = []; // iteration pointers
+	      this.objectOnly = objectOnly;
+
+	      //parse initial iterable argument passed
+	      if (a) init.call(this, a);
+	    }
+
+	    //define size for non object-only collections
+	    if (!objectOnly) {
+	      defineProperty(proto, 'size', {
+	        get: sharedSize
+	      });
+	    }
+
+	    //set prototype
+	    proto.constructor = Collection;
+	    Collection.prototype = proto;
+
+	    return Collection;
+	  }
+
+
+	  /** parse initial iterable argument passed */
+	  function init(a){
+	    var i;
+	    //init Set argument, like `[1,2,3,{}]`
+	    if (this.add)
+	      a.forEach(this.add, this);
+	    //init Map argument like `[[1,2], [{}, 4]]`
+	    else
+	      a.forEach(function(a){this.set(a[0],a[1])}, this);
+	  }
+
+
+	  /** delete */
+	  function sharedDelete(key) {
+	    if (this.has(key)) {
+	      this._keys.splice(i, 1);
+	      this._values.splice(i, 1);
+	      // update iteration pointers
+	      this._itp.forEach(function(p) { if (i < p[0]) p[0]--; });
+	    }
+	    // Aurora here does it while Canary doesn't
+	    return -1 < i;
+	  };
+
+	  function sharedGet(key) {
+	    return this.has(key) ? this._values[i] : undefined;
+	  }
+
+	  function has(list, key) {
+	    if (this.objectOnly && key !== Object(key))
+	      throw new TypeError("Invalid value used as weak collection key");
+	    //NaN or 0 passed
+	    if (key != key || key === 0) for (i = list.length; i-- && !is(list[i], key);){}
+	    else i = list.indexOf(key);
+	    return -1 < i;
+	  }
+
+	  function setHas(value) {
+	    return has.call(this, this._values, value);
+	  }
+
+	  function mapHas(value) {
+	    return has.call(this, this._keys, value);
+	  }
+
+	  /** @chainable */
+	  function sharedSet(key, value) {
+	    this.has(key) ?
+	      this._values[i] = value
+	      :
+	      this._values[this._keys.push(key) - 1] = value
+	    ;
+	    return this;
+	  }
+
+	  /** @chainable */
+	  function sharedAdd(value) {
+	    if (!this.has(value)) this._values.push(value);
+	    return this;
+	  }
+
+	  function sharedClear() {
+	    (this._keys || 0).length =
+	    this._values.length = 0;
+	  }
+
+	  /** keys, values, and iterate related methods */
+	  function sharedKeys() {
+	    return sharedIterator(this._itp, this._keys);
+	  }
+
+	  function sharedValues() {
+	    return sharedIterator(this._itp, this._values);
+	  }
+
+	  function mapEntries() {
+	    return sharedIterator(this._itp, this._keys, this._values);
+	  }
+
+	  function setEntries() {
+	    return sharedIterator(this._itp, this._values, this._values);
+	  }
+
+	  function sharedIterator(itp, array, array2) {
+	    var p = [0], done = false;
+	    itp.push(p);
+	    return {
+	      next: function() {
+	        var v, k = p[0];
+	        if (!done && k < array.length) {
+	          v = array2 ? [array[k], array2[k]]: array[k];
+	          p[0]++;
+	        } else {
+	          done = true;
+	          itp.splice(itp.indexOf(p), 1);
+	        }
+	        return { done: done, value: v };
+	      }
+	    };
+	  }
+
+	  function sharedSize() {
+	    return this._values.length;
+	  }
+
+	  function sharedForEach(callback, context) {
+	    var it = this.entries();
+	    for (;;) {
+	      var r = it.next();
+	      if (r.done) break;
+	      callback.call(context, r.value[1], r.value[0], this);
+	    }
+	  }
+
+	})(typeof exports != 'undefined' && typeof global != 'undefined' ? global : window );
+
+	/* WEBPACK VAR INJECTION */}.call(exports, (function() { return this; }())))
+
+/***/ },
+/* 16 */
+/***/ function(module, exports) {
+
 	module.exports = [
 		{
 			"start_time": "2016-01-18",
@@ -26878,19 +27117,32 @@
 			"map": "tlaza_a"
 		},
 		{
-			"type": "squad_battle",
-			"title": "マグメル機体試験場 〜FIELD-D〜",
+			"start_time": "2016-01-25",
+			"end_time": "2016-01-31",
+			"type": "national_battle_high",
+			"title": "【初公開】旧ブロア市街地〜占有驀進〜【特殊×】",
+			"url": ""
+		},
+		{
+			"start_time": "2016-01-25",
+			"end_time": "2016-01-31",
+			"type": "national_battle_low",
+			"title": "ダリーヤ遺跡群 〜河底の弾雨〜【特殊×】",
+			"url": "/map/5#opr-28",
+			"map": "darya_b"
+		},
+		{
+			"title": "マグメル機体試験場〜FIELD-D〜",
 			"map": "24_sqa"
 		},
 		{
-			"type": "squad_battle",
-			"title": "マグメル機体試験場 〜FIELD-S〜",
+			"title": "マグメル機体試験場〜FIELD-S〜",
 			"map": "24_sqb"
 		}
 	];
 
 /***/ },
-/* 16 */
+/* 17 */
 /***/ function(module, exports) {
 
 	module.exports = [
@@ -27065,7 +27317,7 @@
 	];
 
 /***/ },
-/* 17 */
+/* 18 */
 /***/ function(module, exports) {
 
 	module.exports = [
@@ -28357,14 +28609,14 @@
 	];
 
 /***/ },
-/* 18 */
+/* 19 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
-	var _jquery = __webpack_require__(19);
+	var _jquery = __webpack_require__(20);
 
 	var _jquery2 = _interopRequireDefault(_jquery);
 
@@ -28387,9 +28639,9 @@
 	// require('jquery-ui/themes/base/theme.css');
 	// require('jquery-ui/themes/smoothness/jquery-ui.min.css');
 
-	__webpack_require__(20);
-
 	__webpack_require__(21);
+
+	__webpack_require__(22); /*global require*/ // eslint
 
 	//初期化
 	var CanvasName = "BBCompass";
@@ -29881,7 +30133,7 @@
 	// scrollLimitX, scrollLimitY,
 
 /***/ },
-/* 19 */
+/* 20 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*!
@@ -39097,7 +39349,7 @@
 
 
 /***/ },
-/* 20 */
+/* 21 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*
@@ -39336,19 +39588,19 @@
 
 	})(jQuery);
 
-	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(19)))
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(20)))
 
 /***/ },
-/* 21 */
+/* 22 */
 /***/ function(module, exports, __webpack_require__) {
 
 	// style-loader: Adds some css to the DOM by adding a <style> tag
 
 	// load the styles
-	var content = __webpack_require__(22);
+	var content = __webpack_require__(23);
 	if(typeof content === 'string') content = [[module.id, content, '']];
 	// add the styles to the DOM
-	var update = __webpack_require__(24)(content, {});
+	var update = __webpack_require__(25)(content, {});
 	if(content.locals) module.exports = content.locals;
 	// Hot Module Replacement
 	if(false) {
@@ -39365,10 +39617,10 @@
 	}
 
 /***/ },
-/* 22 */
+/* 23 */
 /***/ function(module, exports, __webpack_require__) {
 
-	exports = module.exports = __webpack_require__(23)();
+	exports = module.exports = __webpack_require__(24)();
 	// imports
 
 
@@ -39379,7 +39631,7 @@
 
 
 /***/ },
-/* 23 */
+/* 24 */
 /***/ function(module, exports) {
 
 	/*
@@ -39435,7 +39687,7 @@
 
 
 /***/ },
-/* 24 */
+/* 25 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/*
