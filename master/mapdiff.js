3270ccc29365bd3560df7cd48fac7931af6212ee
hell0again <hell00again@gmail.com>
2016-07-08 09:53:13 +0900

    [master] merge feature/bbx

--- master/mapdiff.js
+++ master/mapdiff.js
@@ -18938,128 +18938,113 @@ module.exports = warning;
 module.exports = require('./lib/React');
 
 },{"./lib/React":25}],158:[function(require,module,exports){
-'use strict';
-
-var React = require('react');
+'use strict';var React = require('react');
 var ReactDOM = require('react-dom');
-var OreController = React.createClass({
-  displayName: 'OreController',
+var OreController = React.createClass({ displayName: 'OreController', 
   getInitialState: function getInitialState() {
-    return {
-      visibility: "visible",
-      opacity: 0.5,
-      scale: 1.00,
-      x: 0,
-      y: 0,
-      src: "map/24/24_sqa.jpg",
-      srcInput: "map/24/24_sqa.jpg",
-      srcWidth: 599,
-      srcHeight: 1166
-    };
-  },
+    return { 
+      visibility: "visible", 
+      opacity: 0.5, 
+      scale: 1.00, 
+      x: 0, 
+      y: 0, 
+      src: "map/24/24_sqa.jpg", 
+      srcInput: "map/24/24_sqa.jpg", 
+      srcWidth: 599, 
+      srcHeight: 1166 };}, 
+
+
   onChangeVisibility: function onChangeVisibility(e) {
     var state = {};
     state["visibility"] = e.target.checked ? "visible" : "hidden";
-    this.setState(state);
-  },
+    this.setState(state);}, 
+
   onChangeValue: function onChangeValue(e) {
     var state = {};
     state[e.target.dataset["name"]] = e.target.value;
-    this.setState(state);
-  },
+    this.setState(state);}, 
+
   onChangeSrc: function onChangeSrc(e) {
     var img = new Image();
     var that = this;
-    var state = {
-      src: "",
-      srcWidth: 0,
-      srcHeight: 0
-    };
+    var state = { 
+      src: "", 
+      srcWidth: 0, 
+      srcHeight: 0 };
+
     this.setState(state);
     img.onload = function () {
-      var state = {
-        src: img.src,
-        srcWidth: img.width,
-        srcHeight: img.height
-      };
-      that.setState(state);
-    };
+      var state = { 
+        src: img.src, 
+        srcWidth: img.width, 
+        srcHeight: img.height };
+
+      that.setState(state);};
+
     img.src = e.target.value;
-    this.onChangeValue(e);
-  },
+    this.onChangeValue(e);}, 
+
   getWidth: function getWidth() {
     var n = this.state.srcWidth * this.state.scale;
-    return n.toFixed(3);
-  },
+    return n.toFixed(3);}, 
+
   getHeight: function getHeight() {
     var n = this.state.srcHeight * this.state.scale;
-    return n.toFixed(3);
-  },
+    return n.toFixed(3);}, 
+
   getControllerStyle: function getControllerStyle() {
-    var css = {
-      position: "fixed",
-      top: this.props.label == "base" ? 8 : 32,
-      left: 10
-    };
-    return css;
-  },
+    var css = { 
+      position: "fixed", 
+      top: this.props.label == "base" ? 8 : 32, 
+      left: 10 };
+
+    return css;}, 
+
   getTargetStyle: function getTargetStyle() {
     var transform = "";
     transform += "translateX(" + this.state.x + "px) ";
     transform += "translateY(" + this.state.y + "px) ";
     transform += "scale(" + this.state.scale + ") ";
-    var css = {
-      visibility: this.state.visibility,
-      position: "absolute",
-      top: 60,
-      left: 10,
-      opacity: this.state.opacity,
-      transform: transform,
-      WebkitTransform: transform,
-      transformOrigin: "top left",
-      transition: "transform 0.2s",
-      WebkitTransition: "-webkit-transform 0.2s"
-    };
-    return css;
-  },
+    var css = { 
+      visibility: this.state.visibility, 
+      position: "absolute", 
+      top: 60, 
+      left: 10, 
+      opacity: this.state.opacity, 
+      transform: transform, 
+      WebkitTransform: transform, 
+      transformOrigin: "top left", 
+      transition: "transform 0.2s", 
+      WebkitTransition: "-webkit-transform 0.2s" };
+
+    return css;}, 
+
   render: function render() {
-    return React.createElement(
-      'div',
-      { className: 'react-OreController' },
-      React.createElement(
-        'div',
-        { className: 'z-index-controller', style: this.getControllerStyle() },
-        'visibility: ',
-        React.createElement('input', { type: 'checkbox', checked: this.state.visibility == "visible", onChange: this.onChangeVisibility }),
-        'src:        ',
-        React.createElement('input', { type: 'text', value: this.state.srcInput, onChange: this.onChangeSrc, 'data-name': 'srcInput' }),
-        'opacity:    ',
-        React.createElement('input', { className: 'input-number', type: 'number', value: this.state.opacity, step: '0.1', min: '0', max: '1', onChange: this.onChangeValue, 'data-name': 'opacity' }),
-        'x:          ',
-        React.createElement('input', { className: 'input-number', type: 'number', value: this.state.x, step: '1', onChange: this.onChangeValue, 'data-name': 'x' }),
-        'y:          ',
-        React.createElement('input', { className: 'input-number', type: 'number', value: this.state.y, step: '1', onChange: this.onChangeValue, 'data-name': 'y' }),
-        'scale:      ',
-        React.createElement('input', { className: 'input-number', type: 'number', value: this.state.scale, step: '0.001', onChange: this.onChangeValue, 'data-name': 'scale' }),
-        'width:      ',
-        React.createElement('input', { className: 'input-number', type: 'number', value: this.getWidth(), disabled: 'disabled' }),
-        'height:     ',
-        React.createElement('input', { className: 'input-number', type: 'number', value: this.getHeight(), disabled: 'disabled' })
-      ),
-      React.createElement(
-        'div',
-        { className: 'z-index-target', style: this.getTargetStyle() },
-        React.createElement('img', { ref: 'targetImg', src: this.state.src })
-      )
-    );
-  }
-});
-ReactDOM.render(React.createElement(
-  'div',
-  { className: 'react-Root' },
-  React.createElement(OreController, { label: 'base' }),
-  React.createElement(OreController, { label: 'compare' })
-), document.getElementById('content'));
+    return (
+      React.createElement('div', { className: 'react-OreController' }, 
+      React.createElement('div', { className: 'z-index-controller', style: this.getControllerStyle() }, 'visibility: ', 
+      React.createElement('input', { type: 'checkbox', checked: this.state.visibility == "visible", onChange: this.onChangeVisibility }), 'src:        ', 
+      React.createElement('input', { type: 'text', value: this.state.srcInput, onChange: this.onChangeSrc, 'data-name': 'srcInput' }), 'opacity:    ', 
+      React.createElement('input', { className: 'input-number', type: 'number', value: this.state.opacity, step: '0.1', min: '0', max: '1', onChange: this.onChangeValue, 'data-name': 'opacity' }), 'x:          ', 
+      React.createElement('input', { className: 'input-number', type: 'number', value: this.state.x, step: '1', onChange: this.onChangeValue, 'data-name': 'x' }), 'y:          ', 
+      React.createElement('input', { className: 'input-number', type: 'number', value: this.state.y, step: '1', onChange: this.onChangeValue, 'data-name': 'y' }), 'scale:      ', 
+      React.createElement('input', { className: 'input-number', type: 'number', value: this.state.scale, step: '0.001', onChange: this.onChangeValue, 'data-name': 'scale' }), 'width:      ', 
+      React.createElement('input', { className: 'input-number', type: 'number', value: this.getWidth(), disabled: 'disabled' }), 'height:     ', 
+      React.createElement('input', { className: 'input-number', type: 'number', value: this.getHeight(), disabled: 'disabled' })), 
+
+      React.createElement('div', { className: 'z-index-target', style: this.getTargetStyle() }, 
+      React.createElement('img', { ref: 'targetImg', src: this.state.src }))));} });
+
+
+
+
+
+ReactDOM.render(
+React.createElement('div', { className: 'react-Root' }, 
+React.createElement(OreController, { label: 'base' }), 
+React.createElement(OreController, { label: 'compare' })), 
+
+document.getElementById('content'));
 
 },{"react":157,"react-dom":1}],159:[function(require,module,exports){
 // shim for using process in browser
